41918159b5a5530957ce60e8e6093a9ed7575a54
brandon santos <brandonx1123@gmail.com>
2021-02-15 19:31:55 -0500

    MVP done :]

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,34 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const MEMORY_ADD = "MEMORY_ADD";
+export const CLEAR_DATA = "CLEAR_DATA";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+export const MEMORY_R = "MEMORY_R";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperator = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearData = () => {
+  return { type: CLEAR_DATA };
+};
+export const memoryAdd = () => {
+  return { type: MEMORY_ADD };
+};
+
+export const memoryClear = () => {
+  return { type: MEMORY_CLEAR };
+};
+
+export const memoryR = () => {
+  return { type: MEMORY_R };
+};
